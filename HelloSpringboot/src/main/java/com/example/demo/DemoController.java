9efc7d0d5294c8ea88a7703b1fdde6c555c4aed1
Tai <nguyentai3907@gmail.com>
2026-03-04 16:59:20 +0700

    Update HelloSpringboot

--- HelloSpringboot/src/main/java/com/example/demo/DemoController.java
+++ HelloSpringboot/src/main/java/com/example/demo/DemoController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -16,9 +18,32 @@ public class DemoController {
 	}
 	
 	@GetMapping("/vd2")
-	public String getMethodName(HttpServletRequest thamso, ModelMap m) {
-		String tenNhaDuoc = thamso.getParameter("ten");
-		m.addAttribute("name", tenNhaDuoc);
+	public String getVidu2(HttpServletRequest thamso, ModelMap m) {
+		String tenNhanDuoc = thamso.getParameter("ten");
+		String mkNhanDuoc = thamso.getParameter("mk");
+		m.addAttribute("name", tenNhanDuoc);
+		m.addAttribute("pass", mkNhanDuoc);
 		return "index2";
+	}
+	
+	@GetMapping("/vd3")
+	public String getVidu3(@RequestParam("ten") String tenP, @RequestParam("mk") String mkP, ModelMap m) {
+		m.addAttribute("name", tenP);
+		m.addAttribute("pass", mkP);
+		return "index2";
+	}
+	
+	@GetMapping("/login")
+	public String dangNhap() {
+		return "login";
+	}
+	
+	@PostMapping("/vd4")
+	public String getVidu4(HttpServletRequest thamso, ModelMap m) {
+		String tenNhanDuoc = thamso.getParameter("4ten");
+		String mkNhanDuoc = thamso.getParameter("4mk");
+		m.addAttribute("name", tenNhanDuoc);
+		m.addAttribute("pass", mkNhanDuoc);
+		return "index4";
 	}
 }
