9b7394e4c85bfecc96873d93bbbff711830d76cd
Tai <nguyentai3907@gmail.com>
2026-03-05 09:12:43 +0700

    thay ten B1

--- HelloWorldServlet/src/main/java/th/nguyenphananhtai/VDServlet/HelloWorld.java
+++ HelloWorldServlet/src/main/java/th/nguyenphananhtai/VDServlet/HelloWorld.java
@@ -8,29 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Servlet implementation class HelloWorld
- */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public HelloWorld() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter wt = response.getWriter();
 		wt.println("Hello World!!");
-		
 	}
 
 }
