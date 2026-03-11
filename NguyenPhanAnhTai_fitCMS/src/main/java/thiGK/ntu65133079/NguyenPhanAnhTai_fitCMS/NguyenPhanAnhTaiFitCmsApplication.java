27d6daaff2f5fd1d71124c65525e9ab50c285d7d
Tai <nguyentai3907@gmail.com>
2026-03-11 14:37:44 +0700

    Làm thử bài thi giữa kì

---
+++ NguyenPhanAnhTai_fitCMS/src/main/java/thiGK/ntu65133079/NguyenPhanAnhTai_fitCMS/NguyenPhanAnhTaiFitCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenPhanAnhTaiFitCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenPhanAnhTaiFitCmsApplication.class, args);
+	}
+
+}
---
+++ NguyenPhanAnhTai_fitCMS/src/test/java/thiGK/ntu65133079/NguyenPhanAnhTai_fitCMS/NguyenPhanAnhTaiFitCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenPhanAnhTaiFitCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
