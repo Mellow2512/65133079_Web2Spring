380064db605934af340f1fc510b896b9c42c86d3
Tai <nguyentai3907@gmail.com>
2026-03-11 16:01:02 +0700

    Update Images

--- NguyenPhanAnhTai_fitCMS/src/main/java/thiGK/ntu65133079/NguyenPhanAnhTai_fitCMS/controllers/HomeController.java
+++ NguyenPhanAnhTai_fitCMS/src/main/java/thiGK/ntu65133079/NguyenPhanAnhTai_fitCMS/controllers/HomeController.java
@@ -1,13 +1,34 @@
 package thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS.models.Page;
+import thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS.models.Post;
 
 @Controller
 public class HomeController {
 	
-    @GetMapping("/")
-    public String showDashboard() {
-        return "Dashboard"; 
-    }
+	ArrayList<Page> dsTrang = new ArrayList<Page>();
+	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
+	
+	public HomeController() {
+		dsTrang.add(new Page("P01", "Trang chủ", "home, main", "Nội dung trang chủ", "none"));
+		dsTrang.add(new Page("P02", "Giới thiệu", "about, info", "Nội dung giới thiệu", "P01"));
+		dsTrang.add(new Page("P03", "Liên hệ", "contact, phone", "Nội dung liên hệ", "none"));
+	}
+	
+	@GetMapping("/page/all")
+	public String getTatCaTrang(ModelMap m) {
+		m.addAttribute("lstPages", dsTrang);
+		return "allpage";
+	}
+	
+	@GetMapping("/")
+	public String getDashBoard(ModelMap m) {
+		return "Dashboard";
+	}
 }
---
+++ NguyenPhanAnhTai_fitCMS/src/main/java/thiGK/ntu65133079/NguyenPhanAnhTai_fitCMS/models/Post.java
@@ -0,0 +1,45 @@
+package thiGK.ntu65133079.NguyenPhanAnhTai_fitCMS.models;
+
+public class Post {
+	private String id, title, content, categoryId;
+	
+	public Post(String id, String title, String content, String categoryId)
+	{
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.categoryId = categoryId;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+}
